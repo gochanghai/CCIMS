39f23098333bd1af8ff76a5a5780a04f6c1066c5
gochanghai <gochanghai@aliyun.com>
2019-12-21 22:31:57 +0800

    添加 commons

---
+++ ccims-system/src/main/java/com/lch/ccmis/system/CcimsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lch.ccmis.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcimsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcimsSystemApplication.class, args);
+    }
+
+}
