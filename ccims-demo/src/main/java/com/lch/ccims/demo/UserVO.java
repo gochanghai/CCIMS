bed8c0c00b687a1afb5bad8781c80bbbb6b7a19f
gochanghai <gochanghai>
2020-04-24 23:03:49 +0800

    添加 添加字段

--- ccims-demo/src/main/java/com/lch/ccims/demo/UserVO.java
+++ ccims-demo/src/main/java/com/lch/ccims/demo/UserVO.java
@@ -2,6 +2,19 @@ package com.lch.ccims.demo;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class UserVO {
+
+    private String username;
+
+    private String nikeName;
+
+    private List<Long> roleIds = new ArrayList<>();
+
+    private String url;
+
+    private String token;
 }
